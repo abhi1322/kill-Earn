33a68748a5aa0d204e82c75e42cf5c8342e656f4
abhi1322 <abhi1322jii@gmail.com>
2024-04-07 11:31:34 +0530

    auhentication-error-in-tournament section

--- app/tournaments/page.tsx
+++ app/tournaments/page.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 
 const page = () => {
@@ -9,15 +9,3 @@ const page = () => {
 };
 
 export default page;
-=======
-import React from "react";
-
-const page = () => {
-  return <div>
-
-    <h1>sddvsvdfvfdv</h1>
-  </div>;
-};
-
-export default page;
->>>>>>> 75ca16f (commit after the pull)
